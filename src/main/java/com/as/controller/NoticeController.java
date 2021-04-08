5292a149d54a430b1c50a44eb0f0ea08aef91288
Yang, Jaeyong <yjy850146@gmail.com>
2021-04-08 22:11:42 +0900

    공지사항 리스트 회원 페이지 생성 및 컨트롤러 추가

--- src/main/java/com/as/controller/NoticeController.java
+++ src/main/java/com/as/controller/NoticeController.java
@@ -11,17 +11,43 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class NoticeController {
 
 
+	/*공지사항 글쓰기 컨트롤러*/
+    @GetMapping("admin/notice_write")
+    public String notice_front(Model model) {
 
-    @GetMapping("admin/notice_front")
-    public String text1(Model model) {
-        return "admin/notice_front";
+        return "admin/notice_write";
     }
 
-    @PostMapping("admin/notice_front")
-    public String text1(Model model, String title) {
+    @PostMapping("admin/notice_write")
+    public String notice_front(Model model, String title) {
+
+        return "admin/notice_write";
+    }
+
+    /*공지사항 리스트 컨트롤러*/
+    @GetMapping("admin/notice_list")
+    public String notice_list(Model model) {
+        return "admin/notice_list";
+    }
+
+    @PostMapping("admin/notice_list")
+    public String notice_list(Model model, String title) {
 
 
-        return "admin/notice_front";
+        return "admin/notice_list";
+    }
+
+    /*공지사항 리스트 회원 컨트롤러*/
+    @GetMapping("front/notice_list_front")
+    public String notice_list_front(Model model) {
+        return "front/notice_list_front";
+    }
+
+    @PostMapping("front/notice_list_front")
+    public String notice_list_front2(Model model) {
+
+
+        return "front/notice_list_front";
     }
 
 }
