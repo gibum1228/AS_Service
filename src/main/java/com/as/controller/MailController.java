4ae1414bb61bd0f2b8ae3809d3885f4039191aaa
Kim, GiBeom <gibum1228@naver.com>
2021-04-30 01:04:27 +0900

    메일 보내기, 세션으로 키 정보 받기

---
+++ src/main/java/com/as/controller/MailController.java
@@ -0,0 +1,47 @@
+package com.as.controller;
+
+import java.security.Principal;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.as.dto.Member;
+import com.as.mapper.MemberMapper;
+import com.as.service.MailSendService;
+
+@RestController
+public class MailController {
+	@Autowired
+	private MailSendService mss;
+	@Autowired
+	private MemberMapper memberMapper;
+
+	@RequestMapping("/front/mail/verify")
+	public String verify(Principal p, HttpSession session) {
+		Member m = memberMapper.findMember(p.getName());
+
+		String authKey = mss.sendAuthMail(m.getEmail());
+		session.setAttribute("authKey", authKey);
+
+		System.out.println(session);
+
+		return session.getAttribute("authKey").toString();
+	}
+
+	@RequestMapping("/front/mail/show_Session")
+	public String showSession(HttpSession session, @RequestParam Map<String, String> map) {
+		System.out.println(session);
+		System.out.println(session.getAttribute("authKey"));
+
+//		ModelAndView mv = new ModelAndView();
+//		mv.setViewName("/index");
+//
+//		return mv;
+		return session.getAttribute("authKey").toString();
+	}
+}
